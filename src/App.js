acbb550ef57c1653b94ffd934a7f747eff35a297
ArtemTarasenko <artem.t.88@Ya.ru>
2022-01-12 11:20:33 +1000

    init

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react"
+import Customers from "./components/Customers"
+
+function App() {
+    return (
+        <div  style={{maxWidth: '450px', border: '1px solid #dedeed', margin: '2rem', padding: '1rem'}}>
+            <Customers />
+        </div>
+    )
+}
+
+export default App
---
+++ src/components/Customers.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Customers() {
+
+    return (
+        <div>
+            <h3>Customers page</h3>
+            <h4>Date : {new Date().toDateString()}</h4>
+            <p>Mauris in lorem sapien. Nunc ornare, enim ut lacinia eleifend, eros leo faucibus lorem, vitae pellentesque libero velit id felis.</p>
+            <p>Nullam in risus nibh. Ut non diam est. Maecenas nec fringilla massa. Nulla egestas dui id odio aliquet bibendum vel in mi. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.</p>
+            <p>Cras elit sem, eleifend non nulla eu, tincidunt iaculis nunc. Maecenas imperdiet erat ac porta dictum. Cras quis maximus massa.</p>
+        </div>
+    )
+}
